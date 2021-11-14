c8ee41b2b212f2d1a341f091040187322bf9a7e1
Rey <rey0705xxx@gmail.com>
2021-11-13 23:17:40 -0600

    Add unit test

---
+++ SuperHero.UnitTest/Commands/Detail.cs
@@ -0,0 +1,41 @@
+﻿using NUnit.Framework;
+using FluentValidation.TestHelper;
+
+namespace SuperHero.UnitTest.Commands
+{
+    public class Detail
+    {
+        private SuperHerore.Application.Validations.DetailValidator validator;
+        [SetUp]
+        public void Setup()
+        {
+            validator = new SuperHerore.Application.Validations.DetailValidator();
+        }
+
+        [Test]
+        public void ShouldrequiredIdField()
+        {
+            validator = new SuperHerore.Application.Validations.DetailValidator();
+            var command = new SuperHerore.Application.Commands.DetailCommand
+            {
+
+            };
+            var result = validator.TestValidate(command);
+
+            result.ShouldNotHaveValidationErrorFor(p => p.Id);
+        }
+
+        [Test]
+        public void ShouldBeOk()
+        {
+            validator = new SuperHerore.Application.Validations.DetailValidator();
+            var command = new SuperHerore.Application.Commands.DetailCommand
+            {
+                Id = 1
+            };
+            var result = validator.TestValidate(command);
+
+            result.ShouldNotHaveValidationErrorFor(p => p.Id);
+        }
+    }
+}
---
+++ SuperHero.UnitTest/Commands/Search.cs
@@ -0,0 +1,41 @@
+﻿using NUnit.Framework;
+using FluentValidation.TestHelper;
+
+namespace SuperHero.UnitTest.Commands
+{
+    public class Search
+    {
+        private SuperHerore.Application.Validations.SearchValidator validator;
+        [SetUp]
+        public void Setup()
+        {
+            validator = new SuperHerore.Application.Validations.SearchValidator();
+        }
+
+        [Test]
+        public void ShouldrequiredSearchField()
+        {
+            validator = new SuperHerore.Application.Validations.SearchValidator();
+            var command = new SuperHerore.Application.Commands.SearchCommand
+            {
+
+            };
+            var result = validator.TestValidate(command);
+
+            result.ShouldNotHaveValidationErrorFor(p => p.Search);
+        }
+
+        [Test]
+        public void ShouldBeOk()
+        {
+            validator = new SuperHerore.Application.Validations.SearchValidator();
+            var command = new SuperHerore.Application.Commands.SearchCommand
+            {
+                Search = "batman"
+            };
+            var result = validator.TestValidate(command);
+
+            result.ShouldNotHaveValidationErrorFor(p => p.Search);
+        }
+    }
+}
